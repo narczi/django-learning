4172dd2facab2f6d04c8eaa94d2d2a2d3b39166d
narczi <smiechu47@gmail.com>
2022-09-01 19:15:26 +0200

    django groups

---
+++ user_custom_abstractbaseuser/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UserCustomAbstractbaseuserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'user_custom_abstractbaseuser'
